c08daaff915ebf2d941f14ade9329af15dfd2b79
Saurabh Prajapati <saurabh945644@gmail.com>
2021-09-13 10:01:24 +0530

    adding interface

--- app/app.ts
+++ app/app.ts
@@ -45,3 +45,15 @@ const logMessage = (message: string): void => console.log(message);
 function logError(err: string): void {
   console.error(err);
 }
+
+let myResult: Result = {
+  playerName: "Marie",
+  score: 5,
+  problemCount: 5,
+  factor: 7,
+};
+
+let player: Person = {
+  name: "Daniel",
+  formatName: () => "Dan",
+};
